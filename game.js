69f2e44b710c5cc8ba4b2f8af3ce502cc077a0cb
dilsher-kk <luckyds033@gmail.com>
2024-04-22 10:31:17 +0500

    my first commit

---
+++ game.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 6 + 1);
+const gaming = await inquirer.prompt({
+    name: "gm",
+    message: "please guess a number",
+    type: "number"
+});
+if (gaming.gm === randomnumber) {
+    console.log("congralations! you guessed right number");
+}
+else {
+    console.log("you guessed wrong number");
+}
